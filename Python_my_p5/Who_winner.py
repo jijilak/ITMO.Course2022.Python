0ba0474927b2ddb9fd05d3d832e0bd015ff1e7d3
Jiji_Lak <julijalakova@gmail.com>
2022-12-02 02:31:56 +0300

    Part 5

---
+++ Python_my_p5/Who_winner.py
@@ -0,0 +1,6 @@
+from Practice5 import name_imput, steps_game, who_win
+
+igrok1 = name_imput(1)
+igrok2 = name_imput(2)
+score = steps_game(igrok1, igrok2)
+who_win(igrok1, igrok2, score[0], score[1])
